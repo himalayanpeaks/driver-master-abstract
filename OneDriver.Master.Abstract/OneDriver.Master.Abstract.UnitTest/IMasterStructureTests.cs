8353e57e0b8c13233240b1d135f98226e5d8ddc8
charsicoder <83376754+himalayanpeaks@users.noreply.github.com>
2025-04-07 23:22:31 +0200

    Bugfix in contract enforcement test

--- OneDriver.Master.Abstract/OneDriver.Master.Abstract.UnitTest/IMasterStructureTests.cs
+++ OneDriver.Master.Abstract/OneDriver.Master.Abstract.UnitTest/IMasterStructureTests.cs
@@ -5,30 +5,29 @@ namespace OneDriver.Master.Abstract.UnitTest
     public class IMasterStructureTests
     {
         [Fact]
-        public void IMaster_InterfaceSignature_ShouldNotChange()
+        public void IMaster_Interface_ShouldContainExpectedMethodNames()
         {
-            var expected = new List<string>
+            var expected = new[]
             {
-                "OneDriver.Master.Abstract.Contracts.Definition+Error SelectSensorAtPort(Int32)",
-                "Int32 ConnectSensor()",
-                "Int32 DisconnectSensor()",
-                "OneDriver.Master.Abstract.Contracts.Definition+Error UpdateDataFromSensor()",
-                "Void UpdateDataFromAllSensors()",
-                "Int32 ReadParameterFromSensor(System.String, System.String ByRef)",
-                "Int32 ReadParameterFromSensor[T](System.String, T ByRef)",
-                "Int32 WriteParameterToSensor(System.String, System.String)",
-                "Int32 WriteParameterToSensor[T](System.String, T)",
-                "Int32 WriteCommandToSensor(System.String, System.String)",
-                "Int32 WriteCommandToSensor[T](System.String, T)",
-                "System.String GetErrorMessage(Int32)",
-                "System.String[] GetAllParamsFromSensor()",
-                "OneDriver.Master.Abstract.Contracts.Definition+Error LoadDataFromPdb(System.String, Int32, Int32)",
-                "OneDriver.Master.Abstract.Contracts.Definition+Error LoadDataFromPdb(System.String, Int32, System.String ByRef)"
+                "SelectSensorAtPort",
+                "ConnectSensor",
+                "DisconnectSensor",
+                "UpdateDataFromSensor",
+                "UpdateDataFromAllSensors",
+                "ReadParameterFromSensor",
+                "WriteParameterToSensor",
+                "WriteCommandToSensor",
+                "GetErrorMessage",
+                "GetAllParamsFromSensor",
+                "LoadDataFromPdb"
             };
 
-            var actual = typeof(IMaster).GetMethods().Select(m => m.ToString()).ToList();
-            foreach (var exp in expected)
-                Assert.Contains(exp, actual);
+            var actual = typeof(IMaster).GetMethods()
+                           .Select(m => m.Name)
+                           .ToHashSet();
+
+            foreach (var name in expected)
+                Assert.Contains(name, actual);
         }
     }
 }
